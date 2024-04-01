bbb5a10cbaa621c9cad94196f0a5aced6d9de6c1
Mateus Nuci <mateusnucicosta@gmail.com>
2024-04-01 09:47:04 -0300

    need a fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,11 @@
-INSERT INTO tb_categoria(nome) VALUES('Equinos')
-INSERT INTO tb_categoria(nome) VALUES('Bois')
-INSERT INTO tb_categoria(nome) VALUES('Gatos')
 
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Smartphone');
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Smart TV');
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Notebook');
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Tablet');
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Mouse');
+INSERT INTO tb_categoria(id,nome) VALUES(categoria_seq.NEXTVAL,'Teclado');
 
-INSERT INTO tb_produto(nome, descricao, valor, categoria_id) VALUES('Mouse Microsoft', 'Mouse sem fio', 250.0, 1);
-INSERT INTO tb_produto(nome, descricao, valor, categoria_id) VALUES('Smartphone Samsung Galaxy A54 5G', 'Samsung Galaxy A54 5G', 1799.0, 2);
-INSERT INTO tb_produto(nome, descricao, valor, categoria_id) VALUES('Smart TV', 'Smart TV LG LED 65 polegadas', 3999, 3);
-
+INSERT INTO tb_produto(id,nome, descricao, valor, categoria_id) VALUES(produto_seq.NEXTVAL,'Mouse Microsoft', 'Mouse sem fio', 250.0, 5);
+INSERT INTO tb_produto(id,nome, descricao, valor, categoria_id) VALUES(produto_seq.NEXTVAL,'Smartphone Samsung Galaxy A54 5G', 'Samsung Galaxy A54 5G', 1799.0, 1);
+INSERT INTO tb_produto(id,nome, descricao, valor, categoria_id) VALUES(produto_seq.NEXTVAL,'Smart TV', 'Smart TV LG LED 65 polegadas', 3999, 2);
